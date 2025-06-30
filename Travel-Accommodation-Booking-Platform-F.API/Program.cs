7726f3a169d54a6a95f0398692ca2f47afc0f481
AbdullahSholi <groupgroup060@gmail.com>
2025-06-30 16:33:47 +0300

    Setup the project infrastructure
    Setup the project infrastructure with Docker Swarm, AWS EC2, DockerHub, Promethues and Grafana
    
    Setup the project architecture by using Clean Architecture

---
+++ Travel-Accommodation-Booking-Platform-F.API/Program.cs
@@ -0,0 +1,9 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Urls.Clear();
+app.Urls.Add("http://0.0.0.0:80");
+
+app.MapGet("/", () => "Hello from Travel Accommodation API! - Activate CI/CD Pipeline");
+
+app.Run();
