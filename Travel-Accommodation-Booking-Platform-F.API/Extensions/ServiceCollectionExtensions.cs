2f84d3c6e4a08926affefef342a6a3345456ca92
AbdullahSholi <groupgroup060@gmail.com>
2025-07-02 15:59:00 +0300

    Separate each set of related services inside separate extension method

---
+++ Travel-Accommodation-Booking-Platform-F.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using Travel_Accommodation_Booking_Platform_F.Application.Mapping;
+using Travel_Accommodation_Booking_Platform_F.Application.Services.AuthService;
+using Travel_Accommodation_Booking_Platform_F.Application.Services.TokenBlacklistService;
+using Travel_Accommodation_Booking_Platform_F.Application.Utils.Generators;
+using Travel_Accommodation_Booking_Platform_F.Domain.Configurations;
+using Travel_Accommodation_Booking_Platform_F.Domain.Interfaces.Repositories;
+using Travel_Accommodation_Booking_Platform_F.Infrastructure.Persistence;
+using Travel_Accommodation_Booking_Platform_F.Infrastructure.Repositories;
+using Travel_Accommodation_Booking_Platform_F.Utils;
+
+namespace Travel_Accommodation_Booking_Platform_F.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IAuthRepository, AuthRepository>();
+        services.AddScoped<ITokenBlacklistedRepository, TokenBlacklistedRepository>();
+        services.AddScoped<ITokenBlacklistService, TokenBlacklistService>();
+        services.AddAutoMapper(typeof(UserProfile).Assembly);
+        services.Configure<EmailSettings>(services.BuildServiceProvider().GetRequiredService<IConfiguration>().GetSection("EmailSettings"));
+        return services;
+    }
+
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var jwtSettingsSection = configuration.GetSection("Jwt");
+        services.Configure<JwtSettings>(jwtSettingsSection);
+        var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+
+        var secretKey = Environment.GetEnvironmentVariable("SECRET_KEY") ??
+                        throw new InvalidOperationException(CustomMessages.UnSetSecretKey);
+
+        services.AddSingleton<JwtTokenGenerator>(sp =>
+        {
+            var jwtOptions = sp.GetRequiredService<IOptions<JwtSettings>>();
+            return new JwtTokenGenerator(jwtOptions, secretKey);
+        });
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.Events = new JwtBearerEvents
+            {
+                OnTokenValidated = async context =>
+                {
+                    var jti = context.Principal.FindFirst(JwtRegisteredClaimNames.Jti)?.Value;
+                    var blacklistService = context.HttpContext.RequestServices.GetService<ITokenBlacklistService>();
+
+                    if (jti != null && await blacklistService.IsTokenBlacklistedAsync(jti))
+                        context.Fail(CustomMessages.TokenIsBlacklisted);
+                }
+            };
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                RoleClaimType = System.Security.Claims.ClaimTypes.Role
+            };
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddDatabase(this IServiceCollection services)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("TRAVEL_ACCOMMODATION_CONNECTION_STRING") ??
+                               throw new InvalidOperationException(CustomMessages.UnSetConnectionString);
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        return services;
+    }
+    
+    public static IServiceCollection AddRateLimiting(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMemoryCache();
+        services.Configure<IpRateLimitOptions>(configuration.GetSection("IpRateLimiting"));
+        services.AddInMemoryRateLimiting();
+        services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        return services;
+    }
+    
+    public static IServiceCollection AddApiVersioningSetup(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+            options.ReportApiVersions = true;
+        });
+        return services;
+    }
+}
--- Travel-Accommodation-Booking-Platform-F.API/Program.cs
+++ Travel-Accommodation-Booking-Platform-F.API/Program.cs
@@ -1,20 +1,5 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using AspNetCoreRateLimit;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Travel_Accommodation_Booking_Platform_F.Application.Mapping;
-using Travel_Accommodation_Booking_Platform_F.Application.Services.AuthService;
-using Travel_Accommodation_Booking_Platform_F.Application.Services.TokenBlacklistService;
-using Travel_Accommodation_Booking_Platform_F.Application.Utils.Generators;
-using Travel_Accommodation_Booking_Platform_F.Domain.Configurations;
-using Travel_Accommodation_Booking_Platform_F.Domain.Interfaces.Repositories;
-using Travel_Accommodation_Booking_Platform_F.Infrastructure.Persistence;
-using Travel_Accommodation_Booking_Platform_F.Infrastructure.Repositories;
+﻿using AspNetCoreRateLimit;
+using Travel_Accommodation_Booking_Platform_F.Extensions;
 using Travel_Accommodation_Booking_Platform_F.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,75 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddLogging();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-builder.Services.AddScoped<ITokenBlacklistedRepository, TokenBlacklistedRepository>();
-builder.Services.AddScoped<ITokenBlacklistService, TokenBlacklistService>();
-builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
-builder.Services.AddAutoMapper(typeof(UserProfile).Assembly);
 
-builder.Services.AddApiVersioning(options =>
-{
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.ReportApiVersions = true;
-});
+builder.Services.AddLogging();
 
-var jwtSettingsSection = builder.Configuration.GetSection("Jwt");
-builder.Services.Configure<JwtSettings>(jwtSettingsSection);
-var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+builder.Services.AddApplicationServices();
 
-var secretKey = Environment.GetEnvironmentVariable("SECRET_KEY") ??
-                throw new InvalidOperationException(CustomMessages.UnSetSecretKey);
+builder.Services.AddApiVersioningSetup();
 
-builder.Services.AddSingleton<JwtTokenGenerator>(
-    sp =>
-    {
-        var jwtSettings = sp.GetRequiredService<IOptions<JwtSettings>>();
-        return new JwtTokenGenerator(jwtSettings, secretKey);
-    });
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
-var connectionString = Environment.GetEnvironmentVariable("TRAVEL_ACCOMMODATION_CONNECTION_STRING") ??
-                       throw new InvalidOperationException(CustomMessages.UnSetConnectionString);
+builder.Services.AddDatabase();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.Events = new JwtBearerEvents
-    {
-        OnTokenValidated = async (context) =>
-        {
-            var jti = context.Principal.FindFirst(JwtRegisteredClaimNames.Jti)?.Value;
-            var blacklistService = context.HttpContext.RequestServices.GetService<ITokenBlacklistService>();
-
-            if (jti != null && await blacklistService.IsTokenBlacklistedAsync(jti))
-                context.Fail(CustomMessages.TokenIsBlacklisted);
-        }
-    };
-
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings.Issuer,
-        ValidAudience = jwtSettings.Audience,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
-        RoleClaimType = ClaimTypes.Role
-    };
-});
+builder.Services.AddRateLimiting(builder.Configuration);
 
 builder.Services.AddCors();
-
-builder.Services.AddControllers();
-
-builder.Services.AddMemoryCache();
-builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
-builder.Services.AddInMemoryRateLimiting();
-builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
 var app = builder.Build();
 
